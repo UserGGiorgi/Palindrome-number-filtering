0b50cb8845194c7a47bd0c6bca8a74fc86f2b0ac
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2024-08-01 20:48:19 +0300

    Initial commit.

---
+++ PalindromeNumberFiltering/Selector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Concurrent;
+
+namespace PalindromeNumberFiltering;
+
+/// <summary>
+/// A static class containing methods for filtering palindrome numbers from a collection of integers.
+/// </summary>
+public static class Selector
+{
+    /// <summary>
+    /// Retrieves a collection of palindrome numbers from the given list of integers using sequential filtering.
+    /// </summary>
+    /// <param name="numbers">The list of integers to filter.</param>
+    /// <returns>A collection of palindrome numbers.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
+    public static IList<int> GetPalindromeInSequence(IList<int>? numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Retrieves a collection of palindrome numbers from the given list of integers using parallel filtering.
+    /// </summary>
+    /// <param name="numbers">The list of integers to filter.</param>
+    /// <returns>A collection of palindrome numbers.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input list 'numbers' is null.</exception>
+    public static IList<int> GetPalindromeInParallel(IList<int> numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Checks whether the given integer is a palindrome number.
+    /// </summary>
+    /// <param name="number">The integer to check.</param>
+    /// <returns>True if the number is a palindrome, otherwise false.</returns>
+    private static bool IsPalindrome(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Recursively checks whether a positive number is a palindrome.
+    /// </summary>
+    /// <param name="number">The positive number to check.</param>
+    /// <param name="divider">The divider used in the recursive check.</param>
+    /// <returns>True if the positive number is a palindrome, otherwise false.</returns>
+    private static bool IsPositiveNumberPalindrome(int number, int divider)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Gets the number of digits in the given integer.
+    /// </summary>
+    /// <param name="number">The integer to count digits for.</param>
+    /// <returns>The number of digits in the integer.</returns>
+    private static byte GetLength(int number)
+    {
+        throw new NotImplementedException();
+    }
+}
